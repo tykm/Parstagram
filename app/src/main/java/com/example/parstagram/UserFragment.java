ee342b619670b0f3ec61800ca56a91890d482dc2
tykm <tylerkim01@gmail.com>
2022-03-03 21:13:39 -0500

    Logout button and Logout functionality working. Need to improve UI.

--- app/src/main/java/com/example/parstagram/UserFragment.java
+++ app/src/main/java/com/example/parstagram/UserFragment.java
@@ -1,5 +1,6 @@
 package com.example.parstagram;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,6 +10,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.parse.ParseUser;
 
 public class UserFragment extends Fragment {
 
@@ -28,6 +32,18 @@ public class UserFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Setup any handles to view objects here
-        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+        final Button btnLogout = (Button) view.findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ParseUser.logOut();
+                goToLogin();
+            }
+        });
+    }
+
+    public void goToLogin()
+    {
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(intent);
     }
 }
